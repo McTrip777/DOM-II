38873bc7ddff4930231fdb2f926d40c10687bdff
McTrip777 <mcjakedude@gmail.com>
2019-01-29 11:39:46 -0800

    first few events

--- js/index.js
+++ js/index.js
@@ -1,9 +1,20 @@
 // Your code goes here
+    // * [ ] `mouseover`
+	// * [ ] `keydown`
+	// * [ ] `wheel`
+	// * [ ] `drag / drop`
+	// * [ ] `load`
+	// * [ ] `focus`
+	// * [ ] `resize`
+	// * [ ] `scroll`
+	// * [ ] `select`
+    // * [ ] `dblclick`
+    
 
 const homeBtn = document.querySelector('.nav-link1')
 
 homeBtn.addEventListener('mouseover', e =>{
-  console.log('I am a double click')
+  console.log('I am a mouseover')
 });
 
 const h1Key = document.querySelector('.logo-heading')
@@ -12,8 +23,20 @@ h1Key.addEventListener('keydown', e =>{
   console.log('I am a keydown')
 });
 
+const headerImg = document.querySelector('.intro img')
+
+headerImg.addEventListener('wheel', e =>{
+  console.log('I am a wheel')
+});
+
 const aboutUsBtn = document.querySelector('.nav-link2')
 
-aboutUsBtn.addEventListener('wheel', e =>{
-  console.log('I am a wheel')
+aboutUsBtn.addEventListener('click', e =>{
+  console.log('I am a click')
+});
+
+const blogBtn = document.querySelector('.nav-link3')
+
+blogBtn.addEventListener('dblclick', e =>{
+  console.log('I am a double click')
 });
