702bb2eb10db3d1e6076c10ee5a1bd4f00a84dba
McTrip777 <mcjakedude@gmail.com>
2019-01-29 10:06:11 -0800

    first commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
 // Your code goes here
+
+const homeBtn = document.querySelector('.nav-link1')
+
+homeBtn.addEventListener('mouseover', e =>{
+  console.log('I am a double click')
+});
+
+const h1Key = document.querySelector('.logo-heading')
+
+h1Key.addEventListener('keydown', e =>{
+  console.log('I am a keydown')
+});
+
+const aboutUsBtn = document.querySelector('.nav-link2')
+
+aboutUsBtn.addEventListener('wheel', e =>{
+  console.log('I am a wheel')
+});
